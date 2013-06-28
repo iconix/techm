696600741a1029db3687a402affe89f5fc4ba3c6
iconix <narhodes1@gmail.com>
2013-06-27 22:42:57 -0400

    Add property to each topic that provides total freq count of all entities in topic

--- google_news.py
+++ google_news.py
@@ -55,40 +55,46 @@ class section:
             else:  # no particular topics
                 url_str = url.string.encode('ascii', 'ignore')
             self.url = url_str
-
-            self.titles = self.__get_titles(self.url)
-            self.entities = []
+            self.freqs = []
+            self.titles = self.__get_titles_and_summaries(self.url)
+            self.freqs = collections.Counter(self.freqs).most_common()
 
         def __repr__(self):
             return str(self)
 
         def __str__(self):
             # JSON formatted
-            return '{"TOPIC": {"name": "' + str(self.name) + '", "url": "' + str(self.url) + '", "titles": ' + str(self.titles) + '}}'
+            return '{"TOPIC": {"name": "' + str(self.name) + '", "url": "' + str(self.url) + '", "all_topic_entities": ' + json.dumps(dict(self.freqs)) + ', "titles": ' + str(self.titles) + '}}'
 
         """
         :param rss_url: raw string representing Google News RSS feed url
         :returns: article titles for url
         """
-        def get_titles(self, rss_url):
+        def get_titles_and_summaries(self, rss_url):
             feed = feedparser.parse(rss_url)
+            entries = feed.entries
 
             titles = []
 
             # for each article on rss page
-            for i in range(0, len(feed.entries)):
+            for i in range(0, len(entries)):
                 # convert unicode to string
-                title_str = feed.entries[i].title.encode('ascii', 'ignore')
+                title_str = entries[i].title.encode('ascii', 'ignore')
+
+                # avoid incorrect quotation formatting in JSON representation
+                title_str = title_str.replace('"', "'")
 
                 # remove news organization at end of title
-                title_str = title_str[:title_str.index(' - ')]
+                end = title_str.find(' - ')
+                if end != -1:  # substring not found
+                    title_str = title_str[:end]
 
                 entities = self.__get_ner_entities(title_str)
 
                 # count occurrences of each entity (returns list of tuples)
                 entity_freqs = collections.Counter(entities).most_common()
 
-                title = self.__title__(title_str, entity_freqs)
+                title = self.__title__(title_str, entity_freqs, self.freqs)
                 titles.append(title)
 
             return titles
@@ -109,23 +115,22 @@ class section:
             return [item for sublist in entity_values for item in sublist]
 
         class __title__:
-            def __init__(self, title_str, entity_freqs):
+            def __init__(self, title_str, entity_freqs, all_ttopic_freqs):
                 self.title_str = title_str
                 self.entity_freqs = entity_freqs
+
+                # build list of entities for ENTIRE trending topic
+                for e in self.entity_freqs:
+                    all_ttopic_freqs.append(str(e[0])*int(e[1]))
 
             def __repr__(self):
                 return str(self)
 
             def __str__(self):
-                entities = []
-                # turn each tuple into JSON serializable dictionary
-                for e in self.entity_freqs:
-                    entities.append(dict(self.entity_freqs))
-
                 # JSON formatted
-                return '{"TITLE": {"title_str": "' + str(self.title_str) + '", "entities": ' + json.dumps(entities) + '}}'
+                return '{"TITLE": {"title_str": "' + str(self.title_str) + '", "entities": ' + json.dumps(dict(self.entity_freqs)) + '}}'
 
-        __get_titles = get_titles  # private copy
+        __get_titles_and_summaries = get_titles_and_summaries  # private copy
         __get_ner_entities = get_ner_entities  # private copy
 
 
@@ -139,11 +144,11 @@ def set_urls_dict():
     urls_dict['World'] = URL(url_prefix + 'w')
     urls_dict['U.S.'] = URL(url_prefix + 'n')
     urls_dict['Business'] = URL(url_prefix + 'b')
-    '''urls_dict['Technology'] = URL(url_prefix + 'tc')
+    urls_dict['Technology'] = URL(url_prefix + 'tc')
     urls_dict['Entertainment'] = URL(url_prefix + 'e')
     urls_dict['Sports'] = URL(url_prefix + 's')
     urls_dict['Health'] = URL(url_prefix + 'm')
-    urls_dict['Science'] = URL(url_prefix + 'snc')'''
+    urls_dict['Science'] = URL(url_prefix + 'snc')
 
     return urls_dict
 
@@ -163,8 +168,15 @@ def main():
 
     print sections
 
-    #get_ner_entities(sections)
-    #count_occurrences(sections)
+    '''for sec in sections:
+        ttopics = sec.trending_topics
+        for topic in ttopics:
+            titles = topic.titles
+            all_entity_freqs_for_topic = []
+            for t in titles:
+                for e in t.entity_freqs:
+                    all_entity_freqs_for_topic.append(str(e[0])*int(e[1]))
+            print topic.name, collections.Counter(all_entity_freqs_for_topic).most_common()'''
 
 if __name__ == "__main__":
     main()
